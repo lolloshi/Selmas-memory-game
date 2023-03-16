d80baffda344070f1077e91868dd11bd9e8a881e
lolloshi <128001914+lolloshi@users.noreply.github.com>
2023-03-16 17:37:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,159 +1,94 @@
-const images = [
-    'image1.jpg', 'image1.jpg', 'image2.jpg', 'image2.jpg',
-    'image3.jpg', 'image3.jpg', 'image4.jpg', 'image4.jpg',
-    'image5.jpg', 'image5.jpg', 'image6.jpg', 'image6.jpg',
-    'image7.jpg', 'image7.jpg', 'image8.jpg', 'image8.jpg',
-    'image9.jpg', 'image9.jpg', 'image10.jpg', 'image10.jpg',
-    'image11.jpg', 'image11.jpg', 'image12.jpg', 'image12.jpg',
-    'image13.jpg', 'image13.jpg', 'image14.jpg', 'image14.jpg',
-    'image15.jpg', 'image15.jpg'
+const gameBoard = document.getElementById('game-board');
+const cardImages = [
+    // Replace these with your own image URLs and sound files
+    'image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg',
+    'image6.jpg', 'image7.jpg', 'image8.jpg'
 ];
 
-const clickSound = new Audio('click.mp3');
-const matchSound = new Audio('match.mp3');
+const flipSound = new Audio('https://github.com/yourusername/yourrepo/raw/main/flipSound.mp3');
+const matchSound = new Audio('https://github.com/yourusername/yourrepo/raw/main/matchSound.mp3');
 
-let gameBoard = document.querySelector('.game-board');
-let flippedCards = [];
-let matchedCards = [];
-let score = 0;
-let firstClick = true;
+const cards = [];
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false;
 
-function flipCard(card) {
-    if (!card.classList.contains('flipped') && flippedCards.length < 2) {
-        card.classList.add('flipped');
-        flippedCards.push(card);
-
-        clickSound.play(); // Replace playAudio(clickSound); with this line
-
-        if (flippedCards.length === 2) {
-            checkMatch();
-        }
-    }
-}
-
-
-
-const highscoreList = document.getElementById("highscore-list");
-const timeElement = document.getElementById("time");
-let timer;
-let time = 0;
-
-function createCard(img) {
-    let card = document.createElement('div');
+function createCard(imgUrl) {
+    const card = document.createElement('div');
     card.classList.add('card');
+    card.dataset.image = imgUrl;
 
-    let cardInner = document.createElement('div');
-    cardInner.classList.add('card-inner');
+    const img = document.createElement('img');
+    img.src = imgUrl;
+    card.appendChild(img);
 
-    let cardFront = document.createElement('div');
-    cardFront.classList.add('card-front');
-
-    let cardBack = document.createElement('div');
-    cardBack.classList.add('card-back');
-    cardBack.style.backgroundImage = `url(${img})`;
-
-    cardInner.appendChild(cardFront);
-    cardInner.appendChild(cardBack);
-    card.appendChild(cardInner);
-
-    card.addEventListener('click', () => {
-        flipCard(card);
-    });
-
+    card.addEventListener('click', flipCard);
     return card;
 }
 
-
-
-function checkMatch() {
-    const [card1, card2] = flippedCards;
-
-    if (card1.querySelector('.card-front').style.backgroundImage ===
-        card2.querySelector('.card-front').style.backgroundImage) {
-        matchedCards.push(card1, card2);
-        matchSound.play();
-        score += 2;
-    } else {
-        setTimeout(() => {
-            card1.classList.remove('flipped');
-            card2.classList.remove('flipped');
-        }, 1000);
-        if (!firstClick) {
-            score -= 1;
-        }
-    }
-
-    flippedCards = [];
-    firstClick = false;
-
-    if (matchedCards.length === images.length) {
-        clearInterval(timer);
-        saveHighScore(prompt("Enter your name:"), score, time);
-        displayHighScores();
-    }
-}
-
-
-function shuffleArray(array) {
+function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
 
-function getHighScores() {
-    return JSON.parse(localStorage.getItem("highScores")) || [];
-}
+function setupGame() {
+    const imagePairs = [...cardImages, ...cardImages];
+    shuffle(imagePairs);
 
-function saveHighScore(name, score, time) {
-    const highScores = getHighScores();
-    highScores.push({ name, score, time });
-    highScores.sort((a, b) => b.score - a.score || a.time - b.time);
-    highScores.splice(5);
-    localStorage.setItem("highScores", JSON.stringify(highScores));
-}
-
-function displayHighScores() {
-    const highScores = getHighScores();
-    highscoreList.innerHTML = "";
-
-    highScores.forEach(({ name, score, time }, index) => {
-        const li = document.createElement("li");
-        li.textContent = `${index + 1}. ${name} - ${score} points - ${time} s`;
-        highscoreList.appendChild(li);
+    imagePairs.forEach(imgUrl => {
+        const card = createCard(imgUrl);
+        cards.push(card);
+        gameBoard.appendChild(card);
     });
 }
 
-displayHighScores();
+function flipCard() {
+    if (lockBoard || this === firstCard) return;
 
-shuffleArray(images);
+    flipSound.play();
+    this.classList.add('flipped');
+    this.firstChild.style.display = 'block';
 
-images.forEach(img => {
-    const card = createCard(img);
-    gameBoard.appendChild(card);
-});
+    if (!firstCard) {
+        firstCard = this;
+        return;
+    }
 
-timer = setInterval(() => {
-    time++;
-    timeElement.textContent = time;
-}, 1000);
+    secondCard = this;
+    checkForMatch();
+}
 
-// New code added below
+function checkForMatch() {
+    if (firstCard.dataset.image === secondCard.dataset.image) {
+        matchSound.play();
+        disableCards();
+    } else {
+        unflipCards();
+    }
+}
 
-const showPicturesBtn = document.getElementById("show-pictures-btn");
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
 
-showPicturesBtn.addEventListener("click", () => {
-    const cards = document.querySelectorAll(".card");
-    cards.forEach(card => card.classList.add("flipped"));
-
+function unflipCards() {
+    lockBoard = true;
     setTimeout(() => {
-        cards.forEach(card => {
-            if (!matchedCards.includes(card)) {
-                card.classList.remove("flipped");
-            }
-        });
-    }, 500);
+        firstCard.classList.remove('flipped');
+        secondCard.classList.remove('flipped');
+        firstCard.firstChild.style.display = 'none';
+        secondCard.firstChild.style.display = 'none';
 
-showPicturesBtn.disabled = true;
+        resetBoard();
+    }, 1000);
+}
 
-});
+function resetBoard() {
+    [firstCard, secondCard, lockBoard] = [null, null, false];
+}
+
+setupGame();
